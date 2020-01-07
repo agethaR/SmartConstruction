fcfdd5dccc72a2b499683e4cb9de3ce8f996b880
SoleneConsten <solene.consten@edu.ece.fr>
2020-01-07 15:19:27 +0100

    navigation_page_accueil

---
+++ interface/src/container/Navigation.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Row,
+  Col} from 'reactstrap';
+  import './Navigation.css';
+
+class Navigation extends Component {
+  constructor(props) {
+   super(props);
+   this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <header className="barre">
+      <div>
+       <Navbar dark expand="md">
+          <NavbarBrand style={{color: '#919DC4'}} href="/">Smart Construction</NavbarBrand>
+          <NavbarToggler  onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink className="home" style={{color: '#919DC4'}} to="/">Le Projet</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={{color: '#919DC4'}} to="/analyses">Analyses</NavLink>
+              </NavItem>
+              </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+      </header>
+    );
+  }
+}
+
+export default Navigation;
